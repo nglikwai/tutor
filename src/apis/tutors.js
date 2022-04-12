d9fb332a36f8aa5e9ed026c58d1681e7a0018d2a
LIK WAI NG <nglikwai@ymail.com>
2022-04-12 16:06:30 +0800

    a

--- src/apis/tutors.js
+++ src/apis/tutors.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export default axios.create({
     // baseURL: 'https://www.dse00.com/',
-    baseURL: 'http://localhost:4000/' || 'https://www.dse00.com/',
+    baseURL: 'https://www.dse00.com/',
 
 })
